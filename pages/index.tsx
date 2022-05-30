07f89ee4674c9ddc2d9bb27b37e32e0775c856e1
Andrew Roesener <aroesener@gmail.com>
2022-05-29 17:00:00 -0700

    initial commit good starting project setup

---
+++ pages/index.tsx
@@ -0,0 +1,6 @@
+import Head from 'next/head'
+import Image from 'next/image'
+
+export default function HomePage() {
+  return <h1 className="text-3xl font-bold underline">Hello world!!</h1>
+}
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    colors: {
+      raisin: "#231f20ff",
+      gate: "#bb4430ff",
+      mBlue: "#7ebdc2ff",
+      mChampagne: "#f3dfa2ff",
+      linen: "#efe6ddff",
+    },
+    extend: {},
+  },
+  plugins: [],
+};
